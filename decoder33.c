f4a576f6fe2b47ddc962140dededf41a0a2099bc
Taku Goto <info@five-ten-workshop.com>
2023-03-29 17:49:35 +0900

    Create decoder33.c

---
+++ decoder33.c
@@ -0,0 +1,15 @@
+/////////////////////////
+// decode 0x33
+/////////////////////////
+void decoder33(uint64_t *in_data){
+  union Temp{
+    uint8_t dat[8];
+    uint64_t dat_64;
+  }*temp;
+  int i;
+
+  temp = (Temp *)in_data;
+  for(i=0;i<8;i++){
+    temp->dat[i]-=(uint8_t)0x33U;
+  }
+}
